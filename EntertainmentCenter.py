6c29ec318f80110f71f64444c2a9f4f720e19376
luhu <luhu@alibaba-inc.com>
2016-05-05 17:24:56 +0800

    udacity movie trailer website submit

---
+++ EntertainmentCenter.py
@@ -0,0 +1,9 @@
+#coding=UTF-8
+import Media
+
+american_captain = Media.movie("美国队长3","IMAX3D《美国队长3》全方位预告+全画幅对比视频 竟然多...","http://r4.ykimg.com/050C0000572ABC3F67BC3D637C0353C5","https://www.youtube.com/watch?v=dKrVegVI0Us")
+turtle = Media.movie("忍者神龟2:破影而出","忍者神龟2:破影而出","http://r4.ykimg.com/05150000572961D267BC3D7BE40CDDA3","https://www.youtube.com/watch?v=VAbRg7xUtVU")
+monster = Media.movie("魔兽","魔兽","http://r2.ykimg.com/051500005729727867BC3D5E1A0B111E","https://www.youtube.com/watch?v=RhFMIRuHAL4")
+xmen = Media.movie("X-Men: Apocalypse","X-Men: Apocalypse","https://i.ytimg.com/vi_webp/Jer8XjMrUB4/mqdefault.webp","https://www.youtube.com/watch?v=Jer8XjMrUB4")
+
+
---
+++ start.py
@@ -0,0 +1,6 @@
+#coding=UTF-8
+import fresh_tomatoes
+import EntertainmentCenter
+
+moivelist = [EntertainmentCenter.american_captain,EntertainmentCenter.monster,EntertainmentCenter.turtle,EntertainmentCenter.xmen]
+fresh_tomatoes.open_movies_page(moivelist)
